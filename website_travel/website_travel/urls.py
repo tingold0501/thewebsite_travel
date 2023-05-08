f4fd26ea0356004f4a06182f3767d0546007fb3c
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2023-05-08 12:38:32 +0700

    ...

--- website_travel/website_travel/urls.py
+++ website_travel/website_travel/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('home.urls')),
-    path('dashboard/', include('dashboard.urls'))
+    # path('dashboard/', include('dashboard.urls'))
     
   
 ]
