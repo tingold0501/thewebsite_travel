b9e313a4614b8206e78afe055d6bcd48afe9aa14
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2023-05-18 10:01:53 +0700

    ...

--- website_travel/website_travel/urls.py
+++ website_travel/website_travel/urls.py
@@ -3,12 +3,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('login/', include('login.urls')),
-   
-    
-  
+
 ]
