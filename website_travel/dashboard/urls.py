4592433b15deaccb67360d17026cd4d9437d1346
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2023-05-18 23:16:22 +0700

    ....

--- website_travel/dashboard/urls.py
+++ website_travel/dashboard/urls.py
@@ -3,6 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
-    path('', views.charts, name='charts'),
-    path('', views.profile, name='profile'),
 ]
--- website_travel/dashboard/views.py
+++ website_travel/dashboard/views.py
@@ -4,8 +4,3 @@ from django.shortcuts import render
 def dashboard(request):
     return render(request, 'index.html')
 
-def charts(request):
-    return render(request, 'charts-chartjs.html')
-
-def profile(request):
-    return render(request, 'pages-profile.html')
