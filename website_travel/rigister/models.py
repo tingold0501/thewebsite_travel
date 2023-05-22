06763fa01d606d709a245dfed97824503ef0fbb1
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2023-05-22 09:06:56 +0700

    ...

--- website_travel/rigister/models.py
+++ website_travel/rigister/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    confirm_password = models.CharField(max_length=100, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        # Kiểm tra nếu confirm_password được cung cấp và không rỗng
+        if self.confirm_password:
+            self.password = self.confirm_password
+        super().save(*args, **kwargs)   
--- website_travel/rigister/views.py
+++ website_travel/rigister/views.py
@@ -1,5 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User
 
-# Create your views here.
 def rigister(request):
-    return render(request, 'rigister.html')
+    if request.method == 'POST':
+        # Lấy dữ liệu từ request.POST
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        # Kiểm tra xem password và confirm_password có khớp không
+        if password == confirm_password:
+            # Tạo một đối tượng User và lưu vào cơ sở dữ liệu
+            user = User(first_name=first_name, last_name=last_name, email=email, password=password)
+            user.save()
+            
+            all_users = User.objects.all()
+            
+            for user in all_users:
+                print(f"User ID: {user.id}")
+                print(f"First Name: {user.first_name}")
+                print(f"Last Name: {user.last_name}")
+                print(f"Email: {user.email}")
+                print(f"Password: {user.password}")
+                print(f"Confirm Password: {user.confirm_password}")
+                print("-----------")
+        
+            # Chuyển hướng người dùng đến trang thành công hoặc trang chính
+            return redirect('login')  # thay 'success' bằng tên view/template tương ứng
+        else:
+            # Xử lý lỗi nếu password và confirm_password không khớp
+            return render(request, 'rigister.html', {'error': "Password and Confirm Password do not match."})
+    else:
+        # Xử lý khi request.method là GET (truy cập trang đăng ký lần đầu)
+        return render(request, 'rigister.html')
+
+
+
