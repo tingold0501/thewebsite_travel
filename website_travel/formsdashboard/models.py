58be57f515b4f5419edc90177ee6b0cc147cdb7c
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2023-05-22 21:52:01 +0700

    ....

--- website_travel/formsdashboard/models.py
+++ website_travel/formsdashboard/models.py
@@ -2,7 +2,7 @@ from django import forms
 from django.db import models
 
 class Slide(models.Model):
-    image = models.ImageField(upload_to='/img/slide/', null= True)
+    image = models.ImageField(upload_to='../img/slide/', null= True)
     field1 = models.CharField(max_length=100)
     field2 = models.TextField()
 
