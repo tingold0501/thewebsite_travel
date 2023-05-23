9962fcc524f85154c20f200a09272a76b63fbedf
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2023-05-24 00:56:34 +0700

    ........

--- website_travel/bookingdetail/views.py
+++ website_travel/bookingdetail/views.py
@@ -1,6 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from rigister.models import User
+from .models import TourBooking
 
 
-
+@login_required
 def bookingdetail(request):
-    return render(request, 'indexBookingDetail.html' )
+    user_id = request.user.id  # Lấy ID của người dùng đã xác thực từ session
+
+    try:
+        # Truy vấn đối tượng người dùng từ database bằng ID
+        user = User.objects.get(id=user_id)
+        user_email = user.email  # Lấy email của người dùng
+
+        if request.method == 'POST':
+            check_in_date = request.POST.get('check_in_date')
+            check_out_date = request.POST.get('check_out_date')
+            room_quantity = request.POST.get('room_quantity')
+            adults = request.POST.get('adults')
+            children = request.POST.get('children')
+
+            tour = TourBooking()
+            tour.check_in_date = check_in_date
+            tour.check_out_date = check_out_date
+            tour.number_of_rooms = room_quantity
+            tour.number_of_adults = adults
+            tour.number_of_children = children
+            tour.user = user  # Gán đối tượng User cho trường user của đối tượng TourBooking
+            # Lưu đối tượng vào database
+            tour.save()
+
+            # In ra thông tin
+            print("Email:", user_email)
+            print("Check-in Date:", check_in_date)
+            print("Check-out Date:", check_out_date)
+            print("Room Quantity:", room_quantity)
+            print("Adults:", adults)
+            print("Children:", children)
+
+        # Lấy danh sách booking của người dùng
+        user_bookings = TourBooking.objects.filter(user=user)
+
+        return render(request, 'indexBookingDetail.html', {'user_bookings': user_bookings})
+
+    except User.DoesNotExist:
+        # Xử lý khi không tìm thấy người dùng
+        return render(request, 'indexBookingDetail.html', {'error': 'User does not exist'})
--- website_travel/login/views.py
+++ website_travel/login/views.py
@@ -1,33 +1,28 @@
-
+from rigister.models import User
 from django.shortcuts import render, redirect
-from bookingdetail.models import TourBooking
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 
 def user_login(request):
     if request.method == 'POST':
         email = request.POST['email']
         password = request.POST['password']
+        
         try:
-            users = User.objects.filter(email=email, password=password)
-            if users.exists():
-                # Lấy đối tượng User đầu tiên trong danh sách
-                user = users.first()
-                # Lưu email vào session
-                request.session['user_email'] = user.email
-
-                # Thực hiện các hành động sau khi đăng nhập thành công
-                print("Login Success")
-                return redirect('bookingdetail')  # Chuyển hướng đến trang bookingdetail sau khi đăng nhập thành công
-            else:
-                # Xử lý khi thông tin đăng nhập không khớp
-                print("Not Login")
-                return render(request, 'login.html', {'error': 'Invalid email or password'})
+            # Xác thực người dùng
+            user = User.objects.get(email=email, password=password)
+            
+            # Lưu thông tin người dùng vào session
+            request.session['user_email'] = user.email
+            
+            # Thực hiện các hành động sau khi đăng nhập thành công
+            print("Login Success")
+            return redirect('bookingdetail')  # Chuyển hướng đến trang bookingdetail sau khi đăng nhập thành công
         except User.DoesNotExist:
-            # Xử lý khi không tìm thấy User
+            # Xử lý khi không tìm thấy User hoặc thông tin đăng nhập không khớp
+            print("Not Login")
             return render(request, 'login.html', {'error': 'Invalid email or password'})
     else:
         return render(request, 'login.html')
+
 
 
 
